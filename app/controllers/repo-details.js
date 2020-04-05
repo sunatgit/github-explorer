9faec7b606c9e10905e75a4402adc1c9a4449576
Sun S <devspk@outlook.com>
2020-04-05 08:03:35 -0700

    Step3: Dynamic loading

--- app/controllers/repo-details.js
+++ app/controllers/repo-details.js
@@ -1,7 +1,29 @@
 import Controller from '@ember/controller';
+import { action } from "@ember/object";
 import { tracked } from '@glimmer/tracking';
 
 export default class RepoDetailsController extends Controller {
     queryParams = ['limit'];
     @tracked limit = 0;
+
+    @tracked showForks = false;
+    @tracked forks = null;
+  
+    reset(element, args) {
+      args[1].showForks = false;
+      args[1].forks = null;
+    }
+
+    @action
+    async getForks() {
+      if (!this.showForks) {
+        if (!this.forks) {
+          this.model.repo.get('forks').then( (data) => {
+            this.forks = data;
+          });
+        }
+      }
+  
+      this.showForks = !this.showForks;
+    }
 }
